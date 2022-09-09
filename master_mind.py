b2362a19ecafec31e5576f3b1bec54e2e213a80e
Vulcan27 <pierre.luciani75@gmail.com>
2022-09-09 13:06:15 +0200

    fixing game loops end outputs

--- master_mind.py
+++ master_mind.py
@@ -7,15 +7,11 @@ from ascii import title
 pions_combi = ['b', 'r', 'v', 'j', 'm']
 
 print(title)
+print('\n')
+print('''\nVous devez trouver une combinaison de 4 couleurs dans le bon ordre.\n\nLes couleurs possibles sont Bleu('b'), Rouge('r'), Vert('v'), Jaune('j') et Mauve('m').\n\nUne fois votre combinaison entrée, le jeu vous indique par un point blanc que vous avez la bonne couleur mais pas à la bonne place,\npar un point noir que vous avez la bonne couleur à la bonne place et par une croix rouge que vous n'avez pas la bonne couleur.''')
 
-def master_mind():
 
-	def generate_combi():
-		random.shuffle(pions_combi)
-		combi_to_find = pions_combi[:4]
-		return combi_to_find
-    
-	def text_to_emo(char):
+def text_to_emo(char):
 		if char == 'B':
 			char = (f"{BgBrightColor.BLACK}⚪{BgBrightColor.OFF}")
 			return char
@@ -42,81 +38,90 @@ def master_mind():
 			return char
 		else:
 			return char
+	
+def generate_combi():
+	random.shuffle(pions_combi)
+	combi_to_find = pions_combi[:4]
+	table = []
+	return [combi_to_find, table]
 
+combi_to_find = generate_combi()[0]
+table = generate_combi()[1]
+    
+def game(combi_to_find, table):
 
-	combi_to_find = generate_combi()
+	input_play_again = 'n'
 	printed_combi = []
 
-	print('''\nVous devez trouver une combinaison de 4 couleurs dans le bon ordre.\n\nLes couleurs possibles sont Bleu('b'), Rouge('r'), Vert('v'), Jaune('j') et Mauve('m').\n\nUne fois votre combinaison entrée, le jeu vous indique par un point blanc que vous avez la bonne couleur mais pas à la bonne place,\npar un point noir que vous avez la bonne couleur à la bonne place et par une croix rouge que vous n'avez pas la bonne couleur.''')
 	for c in combi_to_find:
 		printed_combi.append(text_to_emo(c))
 		# print(text_to_emo(c), end=' ')
 
-	table = []
-    
-	def game():
-		def combi_proposal():
-			combi = []
-			while True:
-				input_combi = input("\nEntrez une couleur non déja choisie parmi 'b', 'r', 'v', 'j' ou 'm':  ")
-				if input_combi in pions_combi and input_combi not in combi:
-					combi.append(input_combi)
-					[(print(text_to_emo(c), end=' ')) for c in combi]
-					if len(combi) == 4:
-						print('\n')
-						break
-				else:
-					print("Saisissez une lettre comme indiqué")
-					[(print(text_to_emo(c), end=' ')) for c in combi]
-			return combi
 
-		print('\n')
-
-		def combi_check(combi):
-			indication = []
-			for c in combi:
-				if c in combi_to_find and combi.index(c) == combi_to_find.index(c):
-					indication.append('N')
-				elif c in combi_to_find and combi.index(c) != combi_to_find.index(c):
-					indication.append('B')
-				else:
-					indication.append('.')
-			random.shuffle(indication)
-			return indication
-        
-		combi = combi_proposal()
-		checking_res = combi_check(combi)
-
-		table.insert(0, (' ').join(combi) + '    ' + (' ').join(checking_res))
-		for l in table:
-			print('')
-			for c in l:
-				print(text_to_emo(c), end='')
-
+	def combi_proposal():
+		combi = []
 		while True:
-			if len(table) > 5 and checking_res != ['N', 'N', 'N', 'N']:
-				print("\n\nPERDU :(\n")
-				print("\n\nLa combinaison était:\n")
-				[(time.sleep(1), print(text_to_emo(c))) for c in combi_to_find]
-				input_play_again = input("Une autre partie ? Entrez 'o' oui, 'n' non :  ")
-				if input_play_again == 'o':
-					return(master_mind())
-				else:
-					return "AU REVOIR..."
-					
-			elif checking_res == ['N', 'N', 'N', 'N']:
-				print("\n\nLa combinaison était bien" + '  ' + (' ').join(printed_combi))				
-				print(f"\n\n🏅 {Effect.BLINK}{BrightColor.WHITE}GAGNE ! \o/{BrightColor.OFF}{Effect.BLINK_OFF} 😎\n")
-				input_play_again = input("Une autre partie ? Entrez 'o' oui, 'n' non :  ")
-				if input_play_again == 'o':
-					return(master_mind())
-				else:
-					return "AU REVOIR..."
+			input_combi = input("\nEntrez une couleur non déja choisie parmi 'b', 'r', 'v', 'j' ou 'm':  ")
+			if input_combi in pions_combi and input_combi not in combi:
+				combi.append(input_combi)
+				[(print(text_to_emo(c), end=' ')) for c in combi]
+				if len(combi) == 4:
+					print('\n')
+					break
 			else:
-				print(game())
-				return ''
+				print("Saisissez une lettre comme indiqué")
+				[(print(text_to_emo(c), end=' ')) for c in combi]
+		return combi
 
-	print(game())
 
 
-master_mind()
+	def combi_check(combi):
+		indication = []
+		for c in combi:
+			if c in combi_to_find and combi.index(c) == combi_to_find.index(c):
+				indication.append('N')
+			elif c in combi_to_find and combi.index(c) != combi_to_find.index(c):
+				indication.append('B')
+			else:
+				indication.append('.')
+		random.shuffle(indication)
+		return indication
+	
+	combi = combi_proposal()
+	checking_res = combi_check(combi)
+
+	table.insert(0, (' ').join(combi) + '    ' + (' ').join(checking_res))
+	for l in table:
+		print('')
+		for c in l:
+			print(text_to_emo(c), end='')
+	
+	print('\n')
+
+	while True:
+		if len(table) >= 6 and checking_res != ['N', 'N', 'N', 'N']:
+			print("\n\nPERDU :(\n")
+			print("\n\nLa combinaison était:\n")
+			[(time.sleep(1), print(text_to_emo(c))) for c in combi_to_find]
+			input_play_again = input("Une autre partie ? Entrez 'o' oui, 'n' non :  ")
+			if input_play_again == 'o':
+				print(game(combi_to_find= generate_combi()[0], table=generate_combi()[1]))
+			else:
+				return "AU REVOIR ..."
+				
+		elif len(table) <= 6 and checking_res == ['N', 'N', 'N', 'N']:
+			print("\n\nLa combinaison était bien" + '  ' + (' ').join(printed_combi))				
+			print(f"\n\n🏅 {Effect.BLINK}{BrightColor.WHITE}GAGNE ! \o/{BrightColor.OFF}{Effect.BLINK_OFF} 😎\n")
+			input_play_again = input("Une autre partie ? Entrez 'o' oui, 'n' non :  ")
+			if input_play_again == 'o':
+				print(game(combi_to_find= generate_combi()[0], table=generate_combi()[1]))
+			else:
+				return "AU REVOIR ..."
+
+		elif len(table) < 6 and checking_res != ['N', 'N', 'N', 'N']:
+			return(game(combi_to_find, table))
+	
+		return ''
+
+
+print(game(generate_combi()[0], generate_combi()[1]))
